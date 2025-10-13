6191d69b442ee1a12b263cbb23ad29162671f7b3
howling techie <howling.techie@gmail.com>
2025-10-13 13:14:50 +0100

    Added GitHub link to the Home page for easy repository access.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,6 +14,14 @@ export const Home = () => {
             <Showcase/>
             <Portfolio/>
             <Contact/>
+            <section>
+                <div className="container p-16 pt-0 relative min-w-screen flex items-center justify-center">
+                    <a href={"https://github.com/Howling-Techie/orbital-industries-portfolio"}
+                       className="uppercase text-2xl dark:font-['Orbitron'] dark:font-bold dark:text-foreground text-background dark:hover:text-hazard-yellow hover:text-international-orange cursor-pointer">
+                        View this site on GitHub
+                    </a>
+                </div>
+            </section>
         </main>
     );
 };
