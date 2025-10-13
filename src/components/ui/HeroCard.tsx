31f465dd1254082f61807e745d0c2244a53ea0f8
howling techie <howling.techie@gmail.com>
2025-10-13 17:33:29 +0100

    Removed unused `HeroCard` component to reduce codebase clutter.

--- src/components/ui/HeroCard.tsx
+++
@@ -1,7 +0,0 @@
-export const HeroCard = ({children}: { children: React.ReactNode }) => {
-    return (
-        <div className="bg-white border-4 border-black h-max">
-            {children}
-        </div>
-    );
-};
