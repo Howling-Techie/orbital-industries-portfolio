6d35beb1ceb58c9f2d2038b6c005a17c84e7ac6c
howling techie <howling.techie@gmail.com>
2025-10-15 18:04:47 +0100

    Added `vite-plugin-chunk-split` to improve code-splitting and updated Vite configuration accordingly.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import {chunkSplitPlugin} from "vite-plugin-chunk-split";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
+        chunkSplitPlugin({strategy: "unbundle"})
     ],
     base: "/orbital-industries-portfolio/",
 });
